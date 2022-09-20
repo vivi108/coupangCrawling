cdb130bd2288eb87bbf5a7828c53374d85d11044
unknown <118ssun@naver.com>
2022-09-21 01:32:26 +0900

    fix : fake-agent

--- callURL.py
+++ callURL.py
@@ -1,11 +1,25 @@
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from time import sleep
 import requests
+import re
+import pandas as pd
+import numpy as np
+import os
+from fake_useragent import UserAgent
 
 # Headers 원래 필수 값이 아니다. 하지만 쿠팡의 경우 headers 없이 크롤링 했을때는 결과가 한개도 나오지 않는다.
 # 그래서 headers를 위와 같이 설정해주면 된다
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36"}
+
+
+
 def getPageString(url):
-    data = requests.get(url, headers=headers)
-    print(data.content)
+    ua = UserAgent(verify_ssl=False)
+    header = {
+        'User-Agent': ua.random}
+    data = requests.get(url, headers=header)
     return data.content
 
+
+getPageString("https://www.coupang.com")
--- getProducts.py
+++ getProducts.py
@@ -1,34 +1,39 @@
-
 from callURL import getPageString
 from bs4 import BeautifulSoup
+import requests
+import openpyxl
+
 
 def getProducts(string):
-    bsObj = BeautifulSoup(string, "html.parser")
-    ul = bsObj.find("ul", {"id": "productList"}) #아이템 리스트 추출
-    lis = ul.findAll("li", {"class":"baby-product renew-badge"})
-    for item in lis:
-        # url
-        a = item.find("a", {"class": "baby-product-link"}) #리스트의 클래스명
-        url = a.get('href')
-        print("url:", url)
+    bsObj = BeautifulSoup(string, "lxml")
+    print(bsObj.prettify())
+    ul = bsObj.find("ul", {"id": "productList"})  # 아이템 리스트 추출
+    lis = ul.findAll("li", {"class": "baby-product renew-badge"})
 
-        # name
-        div_name = item.find("div", {"class": "name"})
-        name = div_name.getText()
-        print("name:", name)
-
-        # price
-        price = item.find("strong", {"class": "price-value"}).getText()
-        print("price:", price)
-
-        rate = item.find("em", {"class":"rating"})
-        print("rate :", rate)
-
-    print("len(lis)",len(lis))
-    return []
-
-
-url = "https://www.coupang.com/np/categories/178454"
-
-pageString = getPageString(url)
-print(getProducts(pageString))
+    # excel_file = openpyxl.Workbook()
+    # excel_sheet = excel_file.active
+    # excel_sheet.append(['순번', '상품명', '판매가격'])
+    print(lis)
+    # for item in lis:
+    #     # url
+    #     a = item.find("a", {"class": "baby-product-link"}) #리스트의 클래스명
+    #     url = a.get('href')
+    #     print("url:", url)
+    #
+    #     # name
+    #     div_name = item.find("div", {"class": "name"})
+    #     name = div_name.getText()
+    #     print("name:", name)
+    #
+    #     # price
+    #     price = item.find("strong", {"class": "price-value"}).getText()
+    #     print("price:", price)
+    #
+    #     rate = item.find("em", {"class":"rating"}).getText()
+    #     print("rate :", rate)
+    #
+    #     #excel_sheet.append([index + 1, name, price, rate])
+    #
+    # print("len(lis)",len(lis))
+    # # excel_file.save('PRODUCT_LIST.xlsx')
+    # # excel_file.close()
